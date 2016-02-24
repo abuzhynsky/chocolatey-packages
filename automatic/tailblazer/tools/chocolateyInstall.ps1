f7396dcf04c6a25cc0348ceea7c60b66e6dba179
a_buzhynsky <3566526@gmail.com>
2016-02-24 17:37:25 +0300

    Add uninstall action of previous version in install script.

--- automatic/tailblazer/tools/chocolateyInstall.ps1
+++ automatic/tailblazer/tools/chocolateyInstall.ps1
@@ -5,5 +5,9 @@ $desktopPath = [Environment]::GetFolderPath("Desktop")
 $lnkPath = $desktopPath + "\TailBlazer.lnk"
 $exePath = $unzipLocation + "\" + $packageName + ".exe"
 
+Write-Host "Uninstalling previous version"
+Remove-Item $unzipLocation -Force -Recurse -Exclude $unzipLocation\logs -ErrorAction SilentlyContinue
+Write-Host "Done"
+
 Install-ChocolateyZipPackage $packageName $url $unzipLocation
 Install-ChocolateyShortcut -shortcutFilePath $lnkPath -targetPath $exePath
